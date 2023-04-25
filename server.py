5a27b810f7c576d0be6faeb76511df25952e4b19
RiversideRocks <wilest7@gmail.com>
2023-04-25 09:04:03 -0400

    todays partial updates

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 from flask import Flask, Response, render_template, request
 import sys, flask
 import httpagentparser
+import classroom
 
 app = Flask(__name__)
 
@@ -9,6 +10,12 @@ GLOBAL_VERSION = "0.0.1 alpha"
 @app.route('/')
 def index():
     return render_template("index.html", version=GLOBAL_VERSION)
+
+@app.route('/step1')
+def s1():
+    # To do this, I needed to include Python's enumerate function.
+    # https://paste.gg/p/anonymous/4c94ffb214e14e5187cd51c19fea80b9
+    return render_template("selectClasses.html", version=GLOBAL_VERSION, classes=classroom.getAllClasses(), enumerate=enumerate)
 
 @app.route('/dashboard')
 def dash():
